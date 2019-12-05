cdc2622fe65b41ad1b9812ed88a6f9bb3cb4048d
David Carrillo <davexcard@gmail.com>
2019-12-05 15:40:06 -0600

    Updating dup2 exercise.

--- chap1/dup2/dup2.go
+++ chap1/dup2/dup2.go
@@ -24,12 +24,21 @@ func main() {
 			f.Close()
 		}
 	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
 
 func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		if input.Text() == "exit" {
+			break
+		} else {
+			counts[input.Text()]++
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
